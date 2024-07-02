399f1795c67f64c260ed5498014cf4af7357b09e
singhmahesh132 <singh.mahesh132@gmail.com>
2024-07-02 16:07:08 +0530

    added comments testcases

--- src/test/java/com/blogging/app/comments/CommentsRepositoryTest.java
+++ src/test/java/com/blogging/app/comments/CommentsRepositoryTest.java
@@ -1,5 +1,10 @@
 package com.blogging.app.comments;
 
+import com.blogging.app.articles.ArticleEntity;
+import com.blogging.app.articles.ArticlesRepository;
+import com.blogging.app.users.UserEntity;
+import com.blogging.app.users.UserRepositoryTests;
+import com.blogging.app.users.UsersRepository;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +18,34 @@ public class CommentsRepositoryTest {
     @Autowired
     private CommentsRepository commentsRepository;
 
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private ArticlesRepository articlesRepository;
+
     @Test
     @Order(1)
     void can_create_comments(){
+        var author = UserEntity.builder()
+                .username("mahesh")
+                .email("singhm@gmail.com")
+                .build();
+        usersRepository.save(author);
+
+        var article = ArticleEntity.builder()
+                .title("mahesh")
+                .slug("i dont know")
+                .body("my book")
+                .author(author)
+                .build();
+        articlesRepository.save(article);
+
         var comment = CommentEntity.builder()
                 .title("my comment")
                 .body("hello this is my comment")
+                .article(article)
+                .author(author)
                 .build();
 
         commentsRepository.save(comment);
