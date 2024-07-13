a54c7b5e418fd1cc3b55f5106de49fbec0fa84a6
Mahesh Singh <singh.mahesh132@gmail.com>
2024-07-13 17:23:04 +0530

    added error response value object for handling error

---
+++ src/main/java/com/blogging/app/security/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.blogging.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorizeHttpRequest -> authorizeHttpRequest.
+                requestMatchers(HttpMethod.GET,"/*").permitAll().
+                requestMatchers(HttpMethod.POST,"/users/create","users/login").permitAll().
+                requestMatchers("/h2-console/*").permitAll().
+                anyRequest().authenticated());
+        return http.build();
+    }
+}
