05b62c8abb1b8489e1904bdca5ffe6c5679b1bb1
Mahesh Singh <singh.mahesh132@gmail.com>
2024-07-13 17:21:55 +0530

    Implemented spring security and implemented permit function using bean

---
+++ src/main/java/com/blogging/app/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.blogging.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/articles")
+public class ArticleController {
+}
---
+++ src/main/java/com/blogging/app/controller/CommentController.java
@@ -0,0 +1,10 @@
+package com.blogging.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/articles/{article-slug}/comments")
+public class CommentController {
+
+}
